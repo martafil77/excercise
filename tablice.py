1d69390c7ae254aac77009bd2e349d6b8b67d817
Marta Filipowska <m.filipowska@creality.pl>
2017-09-04 15:33:37 +0200

    First commit

---
+++ tablice.py
@@ -0,0 +1,21 @@
+liczby = []
+napisy = []
+imiona = ["Jan", "Edward", "Joanna"]
+
+# Tu umiesc swoj kod
+drugie_imie = None
+
+
+
+
+liczby = []    #czy gdybym dała 1,2, 3 w kwadratowym nawiasie to otrzymałabym to samo? Przy imionach jest to w kwadratowym nawiasie przecież.
+liczby.append(1)
+liczby.append(2)
+liczby.append(3)
+napisy = []
+napisy.append('witaj')
+napisy.append('swiecie')
+imiona = ["Jan", "Edward", "Joanna"]    #Jan to 0
+
+# Tu umiesc swoj kod
+drugie_imie = imiona[1]
