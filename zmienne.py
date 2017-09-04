bcb012cb2c86933c52b935af1d2bc4884f7cdcb8
Marta Filipowska <m.filipowska@creality.pl>
2017-09-04 15:06:28 +0200

    First commit

---
+++ zmienne.py
@@ -0,0 +1,42 @@
+zmienna = 5 #ustalenie że zmianna będzie zwracać wartość piątki (czy można tutaj dać liczbę z kropką? czyli np. 5.8?)
+
+calkowita = 7 #to samo co wyżej, tylko tyczy się jedynie liczb całkowitych.
+
+rzeczywista = 7.5 #przecinek to tutaj jest kropka.
+rzeczywista = float(38) #zamiast słowa rzeczywista mogę dać słowo float i osiągnę to samo. W przypadku liczb calkowitych jest to słowo int
+
+x = 4.3 #ustalenie wartości zmiennej x
+print x #wyświetli moją zmienną x. Przy liczbach nie daje się cudzysłowia
+print '%.20f' % x #tego nie czaję.
+print x
+
+napis = 'witaj' #nie ma róznicy czy da się jeden cudzysłów czy dwa, ale jak się da dwa, to można potem wyświetlić tekst z użyciem pojedynczego cudzysłowia, który się wyświetli.
+napis = "witaj"
+
+napis = "Nie martw sie o 'pojedyncze' cudzyslowy."
+
+jeden = 1
+dwa = 2
+trzy = jeden + dwa
+print trzy #wyświetli mi wartość trzy ustaloną wcześniej jako sumę wartości jeden i dwa, które też zostały ustalone chwilę wcześniej.
+
+witaj = "witaj"
+swiecie = "swiecie"
+witajswiecie = witaj + " " + swiecie #wartość witaj, dodać przerwa (spacja), dodać wartość swiecie
+print witajswiecie #wyświetli mi witaj świecie :)
+
+a, b = 3, 4 #rozumiem, że jak gdzieś napiszę później print a, b to wyświetli mi 3, 4
+
+
+#ZADANIE
+
+# zmien ponizszy kod - treść polecenia
+napis = None
+rzeczywista = None
+calkowita = None
+
+
+    # moja robota niżej.
+    napis = 'witaj'
+    rzeczywista = 10.0
+    calkowita = 20
